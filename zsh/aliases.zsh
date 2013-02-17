a14d18ebcdb887d1a712ce0488673f3933556649
jbranchaud <jbranchaud@gmail.com>
2013-02-17 01:35:59 -0600

    Adding an alias for ls to get colored output.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -14,6 +14,7 @@ alias ...='cd ../..'
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+alias ls="ls ${colorflag}"
 alias l="ls -lah ${colorflag}"
 alias la="ls -AF ${colorflag}"
 alias ll="ls -lFh ${colorflag}"
