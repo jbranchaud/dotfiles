5f31ee5fe25acebe92b65b161f89d94731cc4c05
jbranchaud <jbranchaud@gmail.com>
2015-03-30 17:52:18 -0500

    Trim down the git prompt a little.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,7 +1,7 @@
 autoload colors && colors
 
 PROMPT='
-%{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%}$(hg_prompt_info)$(adv_git_prompt)
+%{$fg[magenta]%}%n%{$reset_color%}@%{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}$(collapse_pwd)%{$reset_color%}$(hg_prompt_info)$(adv_git_prompt)
 $(prompt_char) '
 
 function collapse_pwd {
