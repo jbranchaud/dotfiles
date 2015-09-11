110da7ac0b12ba22fc40c7fef23d3d803271e228
jbranchaud <jbranchaud@gmail.com>
2015-09-11 13:52:20 -0500

    Add the lein-bin and lein-exec plugins.

--- lein/lein.symlink/profiles.clj
+++ lein/lein.symlink/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
-                  [lein-bin "0.3.5"]]
+                  [lein-bin "0.3.5"]
+                  [lein-exec "0.3.5"]]
         :dependencies [[slamhound "1.3.1"]]}}
