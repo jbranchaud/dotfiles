264e362a94883fa3e9c6e22436ac112cb2befd01
jbranchaud <jbranchaud@gmail.com>
2015-11-21 14:12:24 -0600

    Add the lein-try plugin to profiles.

--- lein/lein.symlink/profiles.clj
+++ lein/lein.symlink/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [lein-bin "0.3.5"]
-                  [lein-exec "0.3.5"]]
+                  [lein-exec "0.3.5"]
+                  [lein-try "0.4.3"]]
         :dependencies [[slamhound "1.3.1"]]}}
