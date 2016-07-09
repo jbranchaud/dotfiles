a46a15d3a182b044a2458386a727e87b32bad9c3
jbranchaud <jbranchaud@gmail.com>
2016-07-09 15:32:42 -0500

    Add some plugins to the lein profile

--- lein/lein.symlink/profiles.clj
+++ lein/lein.symlink/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[lein-pprint "1.1.1"]
                   [lein-bin "0.3.5"]
                   [lein-exec "0.3.5"]
-                  [lein-try "0.4.3"]]
+                  [lein-try "0.4.3"]
+                  [org.clojure/math.combinatorics "0.1.1"]
+                  [loco "0.3.1"]]
         :dependencies [[slamhound "1.3.1"]]}}
