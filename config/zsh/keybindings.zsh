6bd23f0e7eddf6b251630c9c27bef757ab0a1750
jbranchaud <jbranchaud@gmail.com>
2026-08-10 11:04:08 -0500

    feat(zsh): allow Ctrl-x Ctrl-e to trigger editor for prompt

--- config/zsh/keybindings.zsh
+++ config/zsh/keybindings.zsh
@@ -17,3 +17,11 @@ bindkey "^[f" forward-word
 bindkey "^[b" backward-word
 
 bindkey '^r' history-incremental-search-backward
+
+# Edit current command line in $EDITOR
+# zsh-vi-mode resets keymaps after init, so bind via its hook (array form, so we
+# don't clobber the zvm_after_init function defined in tv.zsh)
+autoload -Uz edit-command-line
+zle -N edit-command-line
+zvm_after_init_commands+=("bindkey -M viins '^X^E' edit-command-line"
+  "bindkey -M vicmd '^X^E' edit-command-line")
